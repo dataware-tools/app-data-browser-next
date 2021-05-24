8b265adbbfd49248968d43b52487ec8abfb4a153
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-05-24 16:38:12 +0900

    add SamplePage

---
+++ src/components/pages/SamplePage.tsx
@@ -0,0 +1,12 @@
+import Container from "@material-ui/core/Container";
+import { Sample } from "components/molecules/Sample";
+
+const Page = (): JSX.Element => {
+  return (
+    <Container>
+      <Sample sample="sample" />
+    </Container>
+  );
+};
+
+export { Page as SamplePage };
