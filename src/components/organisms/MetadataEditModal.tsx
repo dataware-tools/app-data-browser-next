8ff580f8258066b57343f559821ad9ecdf71b0ed
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-06-03 16:42:25 +0900

    fix to use fetched data

--- src/components/organisms/MetadataEditModal.tsx
+++ src/components/organisms/MetadataEditModal.tsx
@@ -3,7 +3,7 @@ import Dialog from "@material-ui/core/Dialog";
 import { DialogCloseButton } from "components/atoms/DialogCloseButton";
 import { useState, useEffect } from "react";
 import LoadingButton from "@material-ui/lab/LoadingButton";
-import { usePrevious } from "utils/index";
+import { DatabaseConfigType, useGetConfig, usePrevious } from "utils/index";
 import {
   MetadataInputFieldList,
   MetadataInputFieldListProps,
@@ -12,6 +12,7 @@ import { DialogContainer } from "components/atoms/DialogContainer";
 import { DialogBody } from "components/atoms/DialogBody";
 import { DialogToolBar } from "components/atoms/DialogToolBar";
 import { DialogTitle } from "components/atoms/DialogTitle";
+import { useAuth0 } from "@auth0/auth0-react";
 
 type ContainerProps = {
   open: boolean;
@@ -19,17 +20,9 @@ type ContainerProps = {
   create?: boolean;
   data?: MetadataInputFieldListProps["data"];
   error: any;
+  databaseId: string;
   onSubmit: (newRecordInfo: Record<string, unknown>) => Promise<boolean>;
 };
-
-// TODO: inputFields should be fetched from server
-const inputFields = [
-  { name: "record name", necessity: "required" },
-  { name: "required", necessity: "required" },
-  { name: "recommended", necessity: "recommended" },
-  { name: "description", necessity: "optional" },
-  { name: "optional", necessity: "optional" },
-];
 
 const Container = ({
   open,
@@ -37,12 +30,35 @@ const Container = ({
   create,
   data,
   error,
+  databaseId,
   onSubmit,
 }: ContainerProps): JSX.Element => {
+  const { getAccessTokenSilently: getAccessToken } = useAuth0();
   const [isSaving, setIsSaving] = useState(false);
   const [nonFilledRequiredsFields, setNonFilledRequireds] = useState<string[]>(
     []
   );
+
+  const [getConfigRes, getConfigError] = (useGetConfig(getAccessToken, {
+    databaseId,
+  }) as unknown) as [
+    data: DatabaseConfigType | undefined,
+    error: any,
+    cacheKey: string
+  ];
+
+  const record_input_config =
+    getConfigRes?.data_browser_config?.record_input_config;
+  const columns = getConfigRes?.columns;
+  const inputFields =
+    record_input_config && columns
+      ? record_input_config.map((config) => ({
+          ...config,
+          display_name:
+            columns.find((column) => column.name === config.name)
+              ?.display_name || "",
+        }))
+      : null;
 
   const initializeState = () => {
     setIsSaving(false);
@@ -57,63 +73,66 @@ const Container = ({
   }, [open, prevOpen]);
 
   const onSave = async () => {
-    setIsSaving(true);
-    // TODO: fixAPI
-    // const newRecordInfo = recordId ? {} : { record_id: Date.now().toString() };
-    const newRecordInfo = {};
+    if (inputFields) {
+      setIsSaving(true);
+      const newRecordInfo = {};
 
-    const nonFilledRequireds: string[] = [];
-    const nonFilledRecommends: string[] = [];
+      const nonFilledRequireds: string[] = [];
+      const nonFilledRecommends: string[] = [];
 
-    inputFields.forEach((inputField) => {
-      const inputEl = document.getElementById(
-        `RecordEditModalInputFields_${inputField.name.replace(/\s+/g, "")}`
-      ) as HTMLInputElement;
-      if (inputField.necessity === "required" && !inputEl.value) {
-        nonFilledRequireds.push(inputField.name);
-      }
-      if (inputField.necessity === "recommended" && !inputEl.value) {
-        nonFilledRecommends.push(inputField.name);
-      }
-    });
+      inputFields.forEach((inputField) => {
+        const inputEl = document.getElementById(
+          `RecordEditModalInputFields_${inputField.name.replace(/\s+/g, "")}`
+        ) as HTMLInputElement;
+        if (inputField.necessity === "required" && !inputEl.value) {
+          nonFilledRequireds.push(inputField.name);
+        }
+        if (inputField.necessity === "recommended" && !inputEl.value) {
+          nonFilledRecommends.push(inputField.name);
+        }
+      });
 
-    if (nonFilledRequireds.length > 0) {
-      window.alert(`${JSON.stringify(nonFilledRequireds)} is required`);
-      setNonFilledRequireds(nonFilledRequireds);
-      setIsSaving(false);
-      return;
-    }
-
-    if (nonFilledRecommends.length > 0) {
-      if (
-        !window.confirm(
-          `${JSON.stringify(
-            nonFilledRecommends
-          )} is recommended. Are you sure to save?`
-        )
-      ) {
+      if (nonFilledRequireds.length > 0) {
+        window.alert(`${JSON.stringify(nonFilledRequireds)} is required`);
+        setNonFilledRequireds(nonFilledRequireds);
         setIsSaving(false);
         return;
       }
-    }
 
-    inputFields.forEach((inputField) => {
-      const inputEl = document.getElementById(
-        `RecordEditModalInputFields_${inputField.name.replace(/\s+/g, "")}`
-      ) as HTMLInputElement;
-      newRecordInfo[inputField.name] = inputEl.value;
-    });
+      if (nonFilledRecommends.length > 0) {
+        if (
+          !window.confirm(
+            `${JSON.stringify(
+              nonFilledRecommends
+            )} is recommended. Are you sure to save?`
+          )
+        ) {
+          setIsSaving(false);
+          return;
+        }
+      }
 
-    // * onSubmit
-    const isSubmitSucceed = await onSubmit(newRecordInfo);
+      inputFields.forEach((inputField) => {
+        const inputEl = document.getElementById(
+          `RecordEditModalInputFields_${inputField.name.replace(/\s+/g, "")}`
+        ) as HTMLInputElement;
+        newRecordInfo[inputField.name] = inputEl.value;
+      });
 
-    if (!isSubmitSucceed) {
+      // @ts-expect-error this is necessary process
+      if (!("path" in newRecordInfo)) newRecordInfo.path = "";
+
+      // * onSubmit
+      const isSubmitSucceed = await onSubmit(newRecordInfo);
+
+      if (!isSubmitSucceed) {
+        setIsSaving(false);
+        window.alert("save failed. please retry saving");
+        return;
+      }
+
       setIsSaving(false);
-      window.alert("save failed. please retry saving");
-      return;
     }
-
-    setIsSaving(false);
     onClose();
   };
 
@@ -122,10 +141,15 @@ const Container = ({
       <DialogContainer>
         <DialogCloseButton onClick={onClose} />
         <DialogTitle>{create ? "Add" : "Edit"} Record</DialogTitle>
-        {error ? (
+        {error || getConfigError ? (
           <ErrorMessage
-            reason={JSON.stringify(error)}
+            reason={JSON.stringify(error || getConfigError)}
             instruction="please reload this page"
+          />
+        ) : !inputFields || inputFields.length <= 0 ? (
+          <ErrorMessage
+            reason="Input fields is not configured"
+            instruction="please report administrator this error"
           />
         ) : data || create ? (
           <>
