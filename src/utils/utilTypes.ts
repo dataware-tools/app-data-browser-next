28c868732c69a424a9f1c67df71b56c57c23a73a
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-06-03 16:21:46 +0900

    add type for config

--- src/utils/utilTypes.ts
+++ src/utils/utilTypes.ts
@@ -4,4 +4,54 @@ type AwaitType<T> = T extends Promise<infer U>
   ? V
   : T;
 
-export type { AwaitType };
+type DatabaseColumnsConfigType = {
+  name: string;
+  display_name: string;
+  dtype:
+    | "string[]"
+    | "string"
+    | "int"
+    | "float"
+    | "double"
+    | "boolean"
+    | "bool"
+    | "object";
+  aggregation:
+    | "accumulator"
+    | "addToSet"
+    | "avg"
+    | "first"
+    | "last"
+    | "max"
+    | "mergeObjects"
+    | "min"
+    | "push"
+    | "stdDevPop"
+    | "stdDevSamp"
+    | "sum";
+}[];
+type DatabaseIndexColumnsConfigType = string[];
+
+type DataBrowserInputConfigType = {
+  name: string;
+  necessity: "required" | "recommended" | "optional";
+}[];
+type DataBrowserDisplayConfigType = string[];
+
+type DatabaseConfigType = {
+  columns: DatabaseColumnsConfigType;
+  index_columns: DatabaseIndexColumnsConfigType;
+  data_browser_config?: {
+    record_input_config?: DataBrowserInputConfigType;
+    record_display_config?: DataBrowserDisplayConfigType;
+  };
+};
+
+export type {
+  AwaitType,
+  DatabaseConfigType,
+  DatabaseColumnsConfigType,
+  DatabaseIndexColumnsConfigType,
+  DataBrowserInputConfigType,
+  DataBrowserDisplayConfigType,
+};
