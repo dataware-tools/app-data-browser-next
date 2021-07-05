85c48f1e282d46c92c6eb680c12317d1a1d32449
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-07-05 23:10:46 +0900

    Add global state

--- src/globalStates.ts
+++ src/globalStates.ts
@@ -1,5 +1,9 @@
 import { atom, useRecoilValue } from "recoil";
-import { UserActionType } from "utils";
+import {
+  ParamTypeListDatabases,
+  ParamTypeListRecords,
+  UserActionType,
+} from "utils";
 
 export const userActionsState = atom<UserActionType[]>({
   key: "userActionsState",
@@ -8,5 +12,32 @@ export const userActionsState = atom<UserActionType[]>({
 
 export const useIsActionPermitted = (action: UserActionType): boolean => {
   const permittedActions = useRecoilValue(userActionsState);
+  if (!permittedActions) {
+    return false;
+  }
   return permittedActions.some((permitted) => action.startsWith(permitted));
 };
+
+type RecordPaginateState = {
+  page: NonNullable<ParamTypeListRecords["page"]>;
+  perPage: NonNullable<ParamTypeListRecords["perPage"]>;
+  search: NonNullable<ParamTypeListRecords["search"]>;
+  searchKey: NonNullable<ParamTypeListRecords["searchKey"]>;
+};
+
+export const recordPaginateState = atom<RecordPaginateState>({
+  key: "recordPaginateState",
+  default: { page: 1, perPage: 20, search: "", searchKey: [] },
+});
+
+type DatabasePaginateState = {
+  page: NonNullable<ParamTypeListDatabases["page"]>;
+  perPage: NonNullable<ParamTypeListDatabases["perPage"]>;
+  search: NonNullable<ParamTypeListDatabases["search"]>;
+  searchKey: NonNullable<ParamTypeListDatabases["searchKey"]>;
+};
+
+export const databasePaginateState = atom<DatabasePaginateState>({
+  key: "recordPaginateState",
+  default: { page: 1, perPage: 20, search: "", searchKey: [] },
+});
