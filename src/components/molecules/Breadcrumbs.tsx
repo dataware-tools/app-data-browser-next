386d108e0b49d8020cf9fa4dc7f27840597908d6
Yusuke Sakai <y.f.sakai@gmail.com>
2021-07-15 15:35:37 +0900

    Fix link

--- src/components/molecules/Breadcrumbs.tsx
+++ src/components/molecules/Breadcrumbs.tsx
@@ -4,6 +4,7 @@ import Typography from "@material-ui/core/Typography";
 import Breadcrumbs from "@material-ui/core/Breadcrumbs";
 import Link from "@material-ui/core/Link";
 import * as MuiIcons from "@material-ui/icons";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -24,40 +25,54 @@ type ItemType = {
   text: string;
 };
 
-type Props = {
+type BreadcrumbsItemProps = {
+  item: ItemType;
+};
+
+const BreadcrumbsItem = ({ item }: BreadcrumbsItemProps): JSX.Element => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  function handleClick(event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) {
+    event.preventDefault();
+    if (item.link) history.push(item.link);
+  }
+
+  return item.link ? (
+    <Link
+      href="#"
+      color="inherit"
+      className={classes.link}
+      onClick={handleClick}
+    >
+      {item.iconName && (
+        <span className={classes.icon}>
+          {React.createElement(MuiIcons[item.iconName])}
+        </span>
+      )}
+      {item.text}
+    </Link>
+  ) : (
+    <Typography color="textPrimary" className={classes.link}>
+      {item.iconName && (
+        <span className={classes.icon}>
+          {React.createElement(MuiIcons[item.iconName])}
+        </span>
+      )}
+      {item.text}
+    </Typography>
+  );
+};
+
+type ComponentProps = {
   items: ItemType[];
 };
 
-const Component = ({ items }: Props): JSX.Element => {
-  const classes = useStyles();
-
+const Component = ({ items }: ComponentProps): JSX.Element => {
   const breadcrumbItems: JSX.Element[] = items.map(
-    (item: ItemType, index: number) => {
-      return item.link ? (
-        <Link
-          color="inherit"
-          href={item.link}
-          key={index}
-          className={classes.link}
-        >
-          {item.iconName && (
-            <span className={classes.icon}>
-              {React.createElement(MuiIcons[item.iconName])}
-            </span>
-          )}
-          {item.text}
-        </Link>
-      ) : (
-        <Typography color="textPrimary" className={classes.link}>
-          {item.iconName && (
-            <span className={classes.icon}>
-              {React.createElement(MuiIcons[item.iconName])}
-            </span>
-          )}
-          {item.text}
-        </Typography>
-      );
-    }
+    (item: ItemType, index: number) => (
+      <BreadcrumbsItem item={item} key={index} />
+    )
   );
 
   return <Breadcrumbs aria-label="breadcrumb">{breadcrumbItems}</Breadcrumbs>;
