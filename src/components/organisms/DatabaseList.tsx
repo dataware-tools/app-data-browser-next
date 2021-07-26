5d3123e8ff9f9e1118e7d205fcc06b1ac6dae493
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-07-26 16:15:55 +0900

    Support deletting database

--- src/components/organisms/DatabaseList.tsx
+++ src/components/organisms/DatabaseList.tsx
@@ -1,20 +1,36 @@
 import { DatabaseListItemProps } from "components/organisms/DatabaseListItem";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useHistory } from "react-router-dom";
-import { ParamTypeListDatabases, useListDatabases } from "utils";
 import {
+  extractReasonFromFetchError,
+  ParamTypeListDatabases,
+  useListDatabases,
+} from "utils";
+import {
+  confirm,
   ErrorMessage,
   ErrorMessageProps,
+  fetchMetaStore,
   LoadingIndicator,
+  metaStore,
 } from "@dataware-tools/app-common";
-import { DataGrid, GridColumns } from "@material-ui/data-grid";
+import {
+  DataGrid,
+  GridColumns,
+  DataGridProps,
+  GridCellParams,
+} from "@material-ui/data-grid";
+import { produce } from "immer";
+import { useState, useEffect } from "react";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
 
 type Props = {
   error?: ErrorMessageProps;
   columns: GridColumns;
   isFetchComplete: boolean;
   databases: DatabaseListItemProps["database"][];
-  onSelectDatabase: (databaseId: string) => void;
+  onSelectDatabase: DataGridProps["onCellClick"];
 };
 
 type ContainerProps = ParamTypeListDatabases;
@@ -34,7 +50,7 @@ const Component = ({
         <DataGrid
           columns={columns}
           rows={databases}
-          onRowClick={(params) => onSelectDatabase(params.row.database_id)}
+          onCellClick={onSelectDatabase}
           getRowId={(row) => row.database_id}
           hideFooter
           disableColumnMenu
@@ -49,14 +65,66 @@ const Component = ({
 const Container = ({ page, perPage, search }: ContainerProps): JSX.Element => {
   const { getAccessTokenSilently: getAccessToken } = useAuth0();
   const history = useHistory();
+  const [error, setError] = useState<Props["error"] | undefined>(undefined);
 
   const {
     data: listDatabasesRes,
     error: listDatabasesError,
+    mutate: listDatabaseMutate,
   } = useListDatabases(getAccessToken, { page, perPage, search });
 
-  const onSelectDatabase = (databaseId: string) =>
-    history.push(`/databases/${databaseId}/records`);
+  const fetchError = listDatabasesError;
+  useEffect(() => {
+    if (fetchError) {
+      setError({
+        reason: extractReasonFromFetchError(fetchError),
+        instruction: "Please reload this page",
+      });
+    } else {
+      setError(undefined);
+    }
+  }, [fetchError]);
+
+  const onDeleteDatabase = async (data: GridCellParams) => {
+    if (listDatabasesRes) {
+      if (
+        !(await confirm({ title: "Are you sure you want to delete database?" }))
+      ) {
+        return;
+      }
+
+      const newDatabaseList = produce(listDatabasesRes, (draft) => {
+        draft.data.splice(
+          draft.data.findIndex((d) => d.database_id === data.id),
+          1
+        );
+      });
+      listDatabaseMutate(newDatabaseList, false);
+
+      const [deleteDatabaseRes, deleteDatabaseError] = await fetchMetaStore(
+        getAccessToken,
+        metaStore.DatabaseService.deleteDatabase,
+        {
+          databaseId: String(data.id),
+        }
+      );
+
+      if (deleteDatabaseError) {
+        setError({
+          reason: extractReasonFromFetchError(deleteDatabaseError),
+          instruction: "Please reload this page",
+        });
+      } else if (deleteDatabaseRes) {
+        listDatabaseMutate();
+      }
+    }
+  };
+
+  const DeleteButtonFieldName = "__DeleteButton__";
+  const onSelectDatabase: Props["onSelectDatabase"] = (data) => {
+    if (data.field === DeleteButtonFieldName) return;
+    history.push(`/databases/${data.id}/records`);
+  };
 
   const columns: GridColumns = [
     {
@@ -70,20 +138,26 @@ const Container = ({ page, perPage, search }: ContainerProps): JSX.Element => {
       field: "description",
       headerName: "Description",
       flex: 1,
-      valueGetter: (param) => param.row.name || "No description...",
+      valueGetter: (param) => param.row.description || "No description...",
+      sortable: false,
+    },
+    {
+      field: DeleteButtonFieldName,
+      renderCell: (param) => (
+        <IconButton onClick={() => onDeleteDatabase(param)}>
+          <DeleteIcon />
+        </IconButton>
+      ),
+      renderHeader: () => <div />,
       sortable: false,
     },
   ];
-  const error: Props["error"] = listDatabasesError
-    ? {
-        reason: JSON.stringify(listDatabasesError),
-        instruction: "Please reload this page",
-      }
-    : undefined;
   const isFetchComplete = Boolean(!error && listDatabasesRes);
   const databases = listDatabasesRes?.data || [];
+
   return (
     <Component
+      error={error}
       columns={columns}
       databases={databases}
       onSelectDatabase={onSelectDatabase}
