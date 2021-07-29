c5e0db1c21c7c98aaa1561ef53963fe3259bfee6
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-07-29 16:13:47 +0900

    Fix Import Path

--- src/components/atoms/FileMenuItem.tsx
+++ src/components/atoms/FileMenuItem.tsx
@@ -1,7 +1,7 @@
 import MenuItem from "@material-ui/core/MenuItem";
 import { ReactNode, useState } from "react";
 import { Spacer } from "@dataware-tools/app-common";
-import { CircularProgress } from "@material-ui/core";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 type ComponentProps = {
   pending: boolean;
