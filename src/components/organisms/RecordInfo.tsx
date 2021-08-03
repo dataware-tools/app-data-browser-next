5854a05af12b0e434249b01279670f3f2939691b
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-08-03 17:43:50 +0900

    Support toggle how to show record detail

--- src/components/organisms/RecordInfo.tsx
+++ src/components/organisms/RecordInfo.tsx
@@ -3,33 +3,62 @@ import {
   ErrorMessageProps,
   LoadingIndicator,
   metaStore,
+  ToolBar,
 } from "@dataware-tools/app-common";
 import { useGetRecord } from "utils";
 import { useAuth0 } from "@auth0/auth0-react";
 import dynamic from "next/dynamic";
+import { useState } from "react";
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { RecordInfoTable } from "./RecordInfoTable";
 const ReactJson = dynamic(import("react-json-view"), { ssr: false });
 
 type Props = {
   record: metaStore.RecordModel;
   error?: ErrorMessageProps;
   isFetchComplete: boolean;
+  isJsonView: boolean;
+  onToggleIsJsonView: () => void;
 };
 type ContainerProps = { databaseId: string; recordId: string };
-const Component = ({ record, error, isFetchComplete }: Props): JSX.Element => {
+const Component = ({
+  record,
+  error,
+  isFetchComplete,
+  isJsonView,
+  onToggleIsJsonView,
+}: Props): JSX.Element => {
   return (
     <>
       {error ? (
         <ErrorMessage {...error} />
       ) : isFetchComplete ? (
-        <ReactJson
-          src={record}
-          style={{
-            overflowWrap: "break-word",
-          }}
-          displayDataTypes={false}
-          quotesOnKeys={false}
-          collapseStringsAfterLength={80}
-        />
+        <>
+          <ToolBar
+            right={
+              <FormControlLabel
+                control={
+                  <Switch checked={isJsonView} onChange={onToggleIsJsonView} />
+                }
+                label="View raw json"
+              />
+            }
+          />
+          {isJsonView ? (
+            <ReactJson
+              src={record}
+              style={{
+                overflowWrap: "break-word",
+              }}
+              displayDataTypes={false}
+              quotesOnKeys={false}
+              collapseStringsAfterLength={80}
+            />
+          ) : (
+            <RecordInfoTable record={record} />
+          )}
+        </>
       ) : (
         <LoadingIndicator />
       )}
@@ -46,11 +75,17 @@ const Container = ({ databaseId, recordId }: ContainerProps): JSX.Element => {
       recordId,
     }
   );
+  const keyIsJsonView = "isJsonView_RecordInfo";
+  const [isJsonView, setIsJsonView] = useState(
+    localStorage.getItem(keyIsJsonView) === "true"
+  );
 
   const record = getRecordRes || {};
-  record.path = record.path.filter(
-    (path: string) => !["", "/", "./.", "./", "/.", "."].includes(path)
-  );
+  if (Array.isArray(record.path)) {
+    record.path = record.path.filter(
+      (path: string) => !["", "/", "./.", "./", "/.", "."].includes(path)
+    );
+  }
   const error: Props["error"] = getRecordError
     ? {
         reason: JSON.stringify(getRecordError),
@@ -58,12 +93,22 @@ const Container = ({ databaseId, recordId }: ContainerProps): JSX.Element => {
       }
     : undefined;
   const isFetchComplete = Boolean(!error && getRecordRes);
+  const onToggleIsJsonView = () => {
+    setIsJsonView((prev) => {
+      prev
+        ? localStorage.setItem(keyIsJsonView, "false")
+        : localStorage.setItem(keyIsJsonView, "true");
+      return !prev;
+    });
+  };
 
   return (
     <Component
       record={record}
       error={error}
       isFetchComplete={isFetchComplete}
+      isJsonView={isJsonView}
+      onToggleIsJsonView={onToggleIsJsonView}
     />
   );
 };
