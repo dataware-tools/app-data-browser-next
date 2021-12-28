d652c2ad6add3f8648061d46d1b61f39437328f8
Toshimitsu Watanabe <kz86nu@gmail.com>
2021-12-28 16:07:29 +0900

    Replace history when redirecting

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 import { APP_ROUTE } from "utils/index";
 
 const Page = (): JSX.Element => {
-  return <Navigate to={APP_ROUTE.DATABASE_LIST} />;
+  return <Navigate to={APP_ROUTE.DATABASE_LIST} replace />;
 };
 
 export { Page as IndexPage };
