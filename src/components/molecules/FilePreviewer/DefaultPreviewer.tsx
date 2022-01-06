bf18ba981644614eeca094c26471b739116eed38
Yusuke Sakai <y.f.sakai@gmail.com>
2022-01-06 14:42:39 +0900

    Add link to document on DefaultPreviewer

--- src/components/molecules/FilePreviewer/DefaultPreviewer.tsx
+++ src/components/molecules/FilePreviewer/DefaultPreviewer.tsx
@@ -1,3 +1,22 @@
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import Button from "@mui/material/Button";
+import Link from "@mui/material/Link";
+
 export const DefaultPreviewer = (): JSX.Element => {
-  return <div>Not supported</div>;
+  return (
+    <div>
+      <p>Not supported for previewing.</p>
+
+      <Button
+        variant="contained"
+        startIcon={<OpenInNewIcon />}
+        component={Link}
+        href="https://docs.dataware-tools.com/viewer-guide/webuapurikshon#purebydekiru"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        List of supported file types
+      </Button>
+    </div>
+  );
 };
