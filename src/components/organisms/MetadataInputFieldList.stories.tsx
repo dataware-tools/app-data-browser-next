049e9c97f8c8ba5646abed3e11da30c06d059153
Toshimitsu Watanabe <kz86nu@gmail.com>
2022-01-07 15:56:23 +0900

    Fix storybook

--- src/components/organisms/MetadataInputFieldList.stories.tsx
+++ src/components/organisms/MetadataInputFieldList.stories.tsx
@@ -1,42 +1,51 @@
-import { Story } from "@storybook/react";
-import {
-  MetadataInputFieldList,
-  MetadataInputFieldListProps,
-} from "./MetadataInputFieldList";
+import { useForm } from "react-hook-form";
+import { MetadataInputFieldList } from "./MetadataInputFieldList";
 
 export default {
   component: MetadataInputFieldList,
   title: "MetadataInputFieldList",
 };
 
-const Template: Story<MetadataInputFieldListProps> = (args) => (
-  <MetadataInputFieldList {...args} />
-);
-
-export const Default = Template.bind({});
-Default.args = {
-  fields: [
-    {
-      name: "required",
-      display_name: "Required",
-      necessity: "required",
-      dtype: "string",
-      aggregation: "first",
+export const Default = (): JSX.Element => {
+  const { control: formControl } = useForm();
+  const validateRules = {
+    required: {
+      required: {
+        value: true,
+        message: `Required is required`,
+      },
     },
-    {
-      name: "recommended",
-      display_name: "Recommended",
-      necessity: "recommended",
-      dtype: "string",
-      aggregation: "first",
-    },
-    {
-      name: "optional",
-      display_name: "Optional",
-      necessity: "optional",
-      dtype: "string",
-      aggregation: "first",
-    },
-  ],
-  prefixInputElementId: "#",
+    recommended: {},
+    optional: {},
+  };
+  return (
+    <MetadataInputFieldList
+      fields={[
+        {
+          name: "required",
+          display_name: "Required",
+          necessity: "required",
+          dtype: "string",
+          aggregation: "first",
+        },
+        {
+          name: "recommended",
+          display_name: "Recommended",
+          necessity: "recommended",
+          dtype: "string",
+          aggregation: "first",
+        },
+        {
+          name: "optional",
+          display_name: "Optional",
+          necessity: "optional",
+          dtype: "string",
+          aggregation: "first",
+        },
+      ]}
+      formControl={formControl}
+      validateRules={validateRules}
+      prefixInputElementId="#"
+    />
+  );
 };
