4dc13e5ec83add47910b141d2cccbfa997ff6fc6
Yusuke Sakai <y.f.sakai@gmail.com>
2022-08-01 10:28:28 +0900

    Use toast for error on deleting database

--- src/components/organisms/DatabaseDeleteModal.tsx
+++ src/components/organisms/DatabaseDeleteModal.tsx
@@ -3,14 +3,10 @@ import { metaStore } from "@dataware-tools/api-meta-store-client";
 import {
   ConfirmModal,
   ConfirmModalProps,
-  ErrorMessageProps,
-  ErrorMessage,
   Spacer,
-  extractErrorMessageFromFetchError,
 } from "@dataware-tools/app-common";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
-import { useState } from "react";
 import {
   useForm,
   Controller,
@@ -22,13 +18,17 @@ import { useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { ElemCenteringFlexDiv } from "components/atoms/ElemCenteringFlexDiv";
 import { databasePaginateState } from "globalStates";
-import { useListDatabases, fetchMetaStore, APP_ROUTE } from "utils";
+import {
+  enqueueErrorToastForFetchError,
+  useListDatabases,
+  fetchMetaStore,
+  APP_ROUTE,
+} from "utils";
 
 type ConfirmInputType = { databaseId: string };
 type ValidateRuleType = ControllerProps["rules"];
 
 export type DatabaseDeleteModalPresentationProps = {
-  error?: ErrorMessageProps;
   formControl: Control<ConfirmInputType>;
   validateRules: Record<keyof ConfirmInputType, ValidateRuleType>;
   validateErrors: FieldErrors<ConfirmInputType>;
@@ -45,7 +45,6 @@ export type DatabaseDeleteModalProps = {
 };
 
 export const DatabaseDeleteModalPresentation = ({
-  error,
   databaseId,
   onClose,
   formControl,
@@ -60,36 +59,32 @@ export const DatabaseDeleteModalPresentation = ({
       title={`Are you sure you want to delete ${databaseId}?`}
       confirmMode="delete"
       body={
-        error ? (
-          <ErrorMessage {...error} />
-        ) : (
-          <ElemCenteringFlexDiv>
-            <Box component="span" sx={{ color: "error.main" }}>
-              Type "{databaseId}" to confirm:
-            </Box>
-            <Spacer direction="horizontal" size="1rem" />
-            <Controller
-              name="databaseId"
-              control={formControl}
-              rules={validateRules.databaseId}
-              render={({ field }) => {
-                return (
-                  <TextField
-                    {...field}
-                    error={Boolean(validateErrors.databaseId)}
-                    helperText={
-                      validateErrors.databaseId
-                        ? validateErrorMessages.databaseId[
-                            validateErrors.databaseId.type
-                          ]
-                        : undefined
-                    }
-                  />
-                );
-              }}
-            />
-          </ElemCenteringFlexDiv>
-        )
+        <ElemCenteringFlexDiv>
+          <Box component="span" sx={{ color: "error.main" }}>
+            Type "{databaseId}" to confirm:
+          </Box>
+          <Spacer direction="horizontal" size="1rem" />
+          <Controller
+            name="databaseId"
+            control={formControl}
+            rules={validateRules.databaseId}
+            render={({ field }) => {
+              return (
+                <TextField
+                  {...field}
+                  error={Boolean(validateErrors.databaseId)}
+                  helperText={
+                    validateErrors.databaseId
+                      ? validateErrorMessages.databaseId[
+                          validateErrors.databaseId.type
+                        ]
+                      : undefined
+                  }
+                />
+              );
+            }}
+          />
+        </ElemCenteringFlexDiv>
       }
       {...dialogProps}
     />
@@ -108,10 +103,6 @@ export const DatabaseDeleteModal = ({
     handleSubmit,
     formState: { errors: validateErrors },
   } = useForm<ConfirmInputType>();
-  const [error, setError] = useState<
-    DatabaseDeleteModalPresentationProps["error"] | undefined
-  >(undefined);
-
   const { mutate: listDatabaseMutate } = useListDatabases(getAccessToken, {
     page,
     perPage,
@@ -153,12 +144,10 @@ export const DatabaseDeleteModal = ({
         );
 
         if (deleteDatabaseError) {
-          const { reason, instruction } =
-            extractErrorMessageFromFetchError(deleteDatabaseError);
-          setError({
-            reason,
-            instruction,
-          });
+          enqueueErrorToastForFetchError(
+            "Failed to delete database",
+            deleteDatabaseError
+          );
           cancelCloseModal = true;
         } else if (deleteDatabaseRes) {
           listDatabaseMutate();
@@ -178,7 +167,6 @@ export const DatabaseDeleteModal = ({
 
   return (
     <DatabaseDeleteModalPresentation
-      error={error}
       validateErrors={validateErrors}
       validateErrorMessages={validateErrorMessages}
       validateRules={validateRules}
